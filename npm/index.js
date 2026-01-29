91509f5c5c34f30eb44a1977504959c2567e8b91
f12o <gh@f12o.com>
2026-01-29 10:59:23 +0900

    fix: update initMhxFfi to use specific handlers and add corresponding functions

--- npm/index.js
+++ npm/index.js
@@ -1,12 +1,19 @@
 import * as mhx from "../dist/index.js";
 import { initMhxFfi } from "../dist/mhx_ffi.js";
 
-initMhxFfi(mhx);
+initMhxFfi({
+  on_fetch_success: mhx.on_fetch_success,
+  on_fetch_error: mhx.on_fetch_error,
+  on_mutation_observed: mhx.on_mutation_observed,
+});
 
 export const init_mhx = mhx.init_mhx;
 export const process = mhx.process;
 export const handle_event = mhx.handle_event;
 export const get_instance = mhx.get_instance;
 export const version = mhx.version;
+export const on_fetch_success = mhx.on_fetch_success;
+export const on_fetch_error = mhx.on_fetch_error;
+export const on_mutation_observed = mhx.on_mutation_observed;
 
 export default mhx;

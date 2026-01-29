34a3bd2894556bbbadbe075afa98a41701f3fdbf
f12o <gh@f12o.com>
2026-01-29 12:45:57 +0900

    fix: update version to 2026.1.4 and refactor initialization of mhxFfi

--- npm/index.js
+++ npm/index.js
@@ -1,11 +1,14 @@
 import * as mhx from "../dist/index.js";
-import { initMhxFfi } from "../dist/mhx_ffi.js";
+import * as mhxFfi from "../dist/mhx_ffi.js";
 
-initMhxFfi({
-  on_fetch_success: mhx.on_fetch_success,
-  on_fetch_error: mhx.on_fetch_error,
-  on_mutation_observed: mhx.on_mutation_observed,
-});
+globalThis.mhx_ffi = mhxFfi;
+if (mhxFfi.initMhxFfi) {
+  mhxFfi.initMhxFfi({
+    on_fetch_success: mhx.on_fetch_success,
+    on_fetch_error: mhx.on_fetch_error,
+    on_mutation_observed: mhx.on_mutation_observed,
+  });
+}
 
 export const init_mhx = mhx.init_mhx;
 export const process = mhx.process;
